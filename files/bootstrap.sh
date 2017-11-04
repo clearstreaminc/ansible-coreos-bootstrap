5bfe159183506f5b3dfc0a8c74064340602a643d
Neil Mayhew <neil@neil.mayhew.name>
2017-11-04 17:42:48 -0600

    Fix inablity to find pip executable during bootstrap play

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -28,7 +28,9 @@ mkdir -p $HOME/bin
 
 cat > $HOME/bin/python <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
+LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH \
+PATH=$HOME/pypy/bin:$PATH \
+exec $HOME/pypy/bin/pypy "\$@"
 EOF
 
 chmod +x $HOME/bin/python
